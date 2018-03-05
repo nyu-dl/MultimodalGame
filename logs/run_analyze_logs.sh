e5488cac0f13ace090b2c87bc4238c1c1e5a4008
lgraesser <lhgraesser@gmail.com>
2018-03-05 18:29:21 -0500

    Adding log analysis

---
+++ logs/run_analyze_logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./analyze_logs.sh experiments_030318/base_nopool 2
+./analyze_logs.sh experiments_030318/pool3 3
+./analyze_logs.sh experiments_030318/pool4 4
+./analyze_logs.sh experiments_030318/pool5 5
+./analyze_logs.sh experiments_030318/pool6 6
+./analyze_logs.sh experiments_030318/pool6_selfish 6
+./analyze_logs.sh experiments_030318/pool8 8
+./analyze_logs.sh experiments_030318/pool10 10
+./analyze_logs.sh experiments_030318/pool20 20
+./analyze_logs.sh experiments_030318/pool30 30
+./analyze_logs.sh experiments_030318/pool40 40

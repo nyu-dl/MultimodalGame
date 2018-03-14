f218e650a4362f70c756f24863d3427fa5fbad85
lgraesser <lhgraesser@gmail.com>
2018-03-14 12:13:17 -0400

    Code quantization

--- analyze_messages.py
+++ analyze_messages.py
@@ -275,60 +275,75 @@ def calc_entropy_ratio(data, agents, answer_type):
     print(f'Ratio: {mean_e / ent_mean_m}/{mean_e / ent_mean_m_prob}')
 
 
-def add_shape_color_elem(d, m_dict, shape=None, color=None):
+def add_shape_color_elem(d, m_dict, shape=None, color=None, blanks=[]):
     for msg_1, msg_2 in zip(d["msg_1"], d["msg_2"]):
         str_m1 = convert_tensor_to_string(msg_1)
         str_m2 = convert_tensor_to_string(msg_2)
+        inc_1 = False if str_m1 in blanks else True
+        inc_2 = False if str_m2 in blanks else True
         if shape is not None and color is not None:
             m_dict[shape + '_' + color]['count'] += 1
-            m_dict[shape + '_' + color]['1']['all'].append(msg_1.numpy())
-            m_dict[shape + '_' + color]['2']['all'].append(msg_2.numpy())
-            if str_m1 in m_dict[shape + '_' + color]['1']:
-                m_dict[shape + '_' + color]['1'][str_m1] += 1
-            else:
-                m_dict[shape + '_' + color]['1'][str_m1] = 1
-            if str_m2 in m_dict[shape + '_' + color]['2']:
-                m_dict[shape + '_' + color]['2'][str_m2] += 1
-            else:
-                m_dict[shape + '_' + color]['2'][str_m2] = 1
+            if inc_1:
+                m_dict[shape + '_' + color]['1']['all'].append(msg_1.numpy())
+                if str_m1 in m_dict[shape + '_' + color]['1']:
+                    m_dict[shape + '_' + color]['1'][str_m1] += 1
+                else:
+                    m_dict[shape + '_' + color]['1'][str_m1] = 1
+            if inc_2:
+                m_dict[shape + '_' + color]['2']['all'].append(msg_2.numpy())
+                if str_m2 in m_dict[shape + '_' + color]['2']:
+                    m_dict[shape + '_' + color]['2'][str_m2] += 1
+                else:
+                    m_dict[shape + '_' + color]['2'][str_m2] = 1
         if shape is not None:
             m_dict[shape]['count'] += 1
-            m_dict[shape]['1']['all'].append(msg_1.numpy())
-            m_dict[shape]['2']['all'].append(msg_2.numpy())
-            if str_m1 in m_dict[shape]['1']:
-                m_dict[shape]['1'][str_m1] += 1
-            else:
-                m_dict[shape]['1'][str_m1] = 1
-            if str_m2 in m_dict[shape]['2']:
-                m_dict[shape]['2'][str_m2] += 1
-            else:
-                m_dict[shape]['2'][str_m2] = 1
+            if inc_1:
+                m_dict[shape]['1']['all'].append(msg_1.numpy())
+                if str_m1 in m_dict[shape]['1']:
+                    m_dict[shape]['1'][str_m1] += 1
+                else:
+                    m_dict[shape]['1'][str_m1] = 1
+            if inc_2:
+                m_dict[shape]['2']['all'].append(msg_2.numpy())
+                if str_m2 in m_dict[shape]['2']:
+                    m_dict[shape]['2'][str_m2] += 1
+                else:
+                    m_dict[shape]['2'][str_m2] = 1
         if color is not None:
             m_dict[color]['count'] += 1
-            m_dict[color]['1']['all'].append(msg_1.numpy())
-            m_dict[color]['2']['all'].append(msg_2.numpy())
-            if str_m1 in m_dict[color]['1']:
-                m_dict[color]['1'][str_m1] += 1
-            else:
-                m_dict[color]['1'][str_m1] = 1
-            if str_m2 in m_dict[color]['2']:
-                m_dict[color]['2'][str_m2] += 1
-            else:
-                m_dict[color]['2'][str_m2] = 1
+            if inc_1:
+                m_dict[color]['1']['all'].append(msg_1.numpy())
+                if str_m1 in m_dict[color]['1']:
+                    m_dict[color]['1'][str_m1] += 1
+                else:
+                    m_dict[color]['1'][str_m1] = 1
+            if inc_2:
+                m_dict[color]['2']['all'].append(msg_2.numpy())
+                if str_m2 in m_dict[color]['2']:
+                    m_dict[color]['2'][str_m2] += 1
+                else:
+                    m_dict[color]['2'][str_m2] = 1
     return m_dict
 
 
-def study_shape_color(data, shape, color):
+def study_shape_color(data, shape, color, blanks=[]):
     m_dict = {shape: {'1': {'all': []}, '2': {'all': []}, 'count': 0},
               color: {'1': {'all': []}, '2': {'all': []}, 'count': 0},
               shape + '_' + color: {'1': {'all': []}, '2': {'all': []}, 'count': 0}}
+    skip_1 = 0
+    skip_2 = 0
     for _, d in enumerate(data):
         if d['shape'] == shape and d['color'] == color:
-            m_dict = add_shape_color_elem(d, m_dict, shape=shape, color=color)
+            if convert_tensor_to_string(d['msg_1'][0]) in blanks:
+                skip_1 += 1
+            if convert_tensor_to_string(d['msg_2'][0]) in blanks:
+                skip_2 += 1
+            m_dict = add_shape_color_elem(d, m_dict, shape=shape, color=color, blanks=blanks)
         elif d['shape'] == shape:
-            m_dict = add_shape_color_elem(d, m_dict, shape=shape, color=None)
+            m_dict = add_shape_color_elem(d, m_dict, shape=shape, color=None, blanks=blanks)
         elif d['color'] == color:
-            m_dict = add_shape_color_elem(d, m_dict, shape=None, color=color)
+            m_dict = add_shape_color_elem(d, m_dict, shape=None, color=color, blanks=blanks)
+    # print(f'Skipped {skip_1}/{skip_2} messages for {shape}_{color}')
     '''Calc mean and std of messages per shape, color and shape color'''
     m_dict[color]['1']['all'] = np.stack(m_dict[color]['1']['all'])
     m_dict[color]['2']['all'] = np.stack(m_dict[color]['2']['all'])
@@ -355,7 +370,7 @@ def study_shape_color(data, shape, color):
     m_dict[shape]['2']['std'] = np.std(m_dict[shape]['2']['all'], axis=0)
     del m_dict[shape]['2']['all']
 
-    # print(f"Number {shape}_{color}: {m_dict[shape + '_' + color]['1']['all'].shape}")
+    print(f"Number {shape}_{color}: {m_dict[shape + '_' + color]['1']['all'].shape}/{m_dict[shape + '_' + color]['2']['all'].shape}")
     m_dict[shape + '_' + color]['1']['mean'] = np.mean(m_dict[shape + '_' + color]['1']['all'], axis=0)
     m_dict[shape + '_' + color]['1']['std'] = np.std(m_dict[shape + '_' + color]['1']['all'], axis=0)
     del m_dict[shape + '_' + color]['1']['all']
@@ -366,8 +381,24 @@ def study_shape_color(data, shape, color):
     return m_dict
 
 
-def create_shape_color_combos(data):
+def quantize_codes(codes):
+    codes = codes.numpy()
+    codes[np.where(codes > 0.95)] = 11
+    codes[np.where(codes >= 0.8, np.where(codes < 0.95, True, False), False)] = 1
+    codes[np.where(codes <= 0.05)] = -1
+    codes[np.where(codes <= 0.2, np.where(codes > 0.05, True, False), False)] = 0
+    codes[np.where(codes > 0.2, np.where(codes < 0.8, True, False), False)] = np.nan
+    codes = torch.from_numpy(codes)
+    return codes
+
+
+def create_shape_color_combos(data, blanks=[]):
+    print(f'Blanks: {blanks}')
     print('================================== SHAPES ==================================')
+    codes_c1 = []
+    codes_c2 = []
+    codes_s1 = []
+    codes_s2 = []
     for s in SHAPES:
         mean_1 = []
         mean_2 = []
@@ -377,7 +408,7 @@ def create_shape_color_combos(data):
                 continue
             sc = s + '_' + c
             cols.append(c)
-            result = study_shape_color(data, s, c)
+            result = study_shape_color(data, s, c, blanks=blanks)
             mean_1.append(result[sc]['1']['mean'])
             mean_2.append(result[sc]['2']['mean'])
         if len(mean_1) > 0:
@@ -385,8 +416,12 @@ def create_shape_color_combos(data):
             print(f'Mean messages for {s}: colors: {cols}')
             mean_1 = torch.from_numpy(np.stack(mean_1))
             mean_2 = torch.from_numpy(np.stack(mean_2))
+            c_1 = torch.mean(mean_1, dim=0)
+            c_2 = torch.mean(mean_2, dim=0)
             print(f'Agent 1: {mean_1}')
             print(f'Agent 2: {mean_2}')
+            codes_s1.append((s, c_1))
+            codes_s2.append((s, c_2))
     print('================================== COLORS ==================================')
     for c in COLORS:
         mean_1 = []
@@ -397,7 +432,7 @@ def create_shape_color_combos(data):
                 continue
             sc = s + '_' + c
             sh.append(s)
-            result = study_shape_color(data, s, c)
+            result = study_shape_color(data, s, c, blanks=blanks)
             mean_1.append(result[sc]['1']['mean'])
             mean_2.append(result[sc]['2']['mean'])
         if len(mean_1) > 0:
@@ -405,8 +440,38 @@ def create_shape_color_combos(data):
             print(f'Mean messages for {c}: shapes: {sh}')
             mean_1 = torch.from_numpy(np.stack(mean_1))
             mean_2 = torch.from_numpy(np.stack(mean_2))
+            c_1 = torch.mean(mean_1, dim=0)
+            c_2 = torch.mean(mean_2, dim=0)
             print(f'Agent 1: {mean_1}')
             print(f'Agent 2: {mean_2}')
+            codes_c1.append((c, c_1))
+            codes_c2.append((c, c_2))
+    shapes = [x[0] for x in codes_s1]
+    print(f'Shapes: {shapes}')
+    shape_codes_1 = [x[1] for x in codes_s1]
+    shape_codes_mean_1 = torch.from_numpy(np.stack(shape_codes_1))
+    print(f'Mean shape codes, agent 1: {shape_codes_mean_1}')
+    shape_codes_2 = [x[1] for x in codes_s2]
+    shape_codes_mean_2 = torch.from_numpy(np.stack(shape_codes_2))
+    print(f'Mean shape codes, agent 2: {shape_codes_mean_2}')
+    colors = [x[0] for x in codes_c1]
+    print(f'Colors: {colors}')
+    color_codes_1 = [x[1] for x in codes_c1]
+    color_codes_mean_1 = torch.from_numpy(np.stack(color_codes_1))
+    print(f'Mean color codes, agent 1: {color_codes_mean_1}')
+    color_codes_2 = [x[1] for x in codes_c2]
+    color_codes_mean_2 = torch.from_numpy(np.stack(color_codes_2))
+    print(f'Mean color codes, agent 2: {color_codes_mean_2}')
+    shape_codes_mean_1 = quantize_codes(shape_codes_mean_1)
+    print(f'Shapes: {shapes}')
+    print(f'Quantized shape codes, agent 1: {shape_codes_mean_1}')
+    shape_codes_mean_2 = quantize_codes(shape_codes_mean_2)
+    print(f'Quantized shape codes, agent 2: {shape_codes_mean_2}')
+    color_codes_mean_1 = quantize_codes(color_codes_mean_1)
+    print(f'Colors: {colors}')
+    print(f'Quantized color codes, agent 1: {color_codes_mean_1}')
+    color_codes_mean_2 = quantize_codes(color_codes_mean_2)
+    print(f'Quantized color codes, agent 2: {color_codes_mean_2}')
 
 
 def print_shape_color_results(result, shape, color, sc):
@@ -489,6 +554,8 @@ def analyze_messages(data):
     result = study_shape_color(data, shape, color)
     pprint.pprint(result)
     create_shape_color_combos(data)
+    print('================================== Shape color studies, no blank msg ==================================')
+    create_shape_color_combos(data, blanks=[list_dict[0][0], list_dict_2[0][0]])
 
 
 def check_distribution(data):
